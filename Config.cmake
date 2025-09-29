80450153ff33183cba38eb8f49ee28e9d3543837
ZilverBlade <90721817+ZilverBlade@users.noreply.github.com>
2025-09-29 09:35:03 +0200

    Changed default BUILD_TESTS to false

--- Config.cmake
+++ Config.cmake
@@ -1,3 +1,3 @@
 # ==== Test config ====
-option(PYRO_COMMON_BUILD_TESTS "Build tests" ON) 
+option(PYRO_COMMON_BUILD_TESTS "Build tests" OFF) 
 option(PYRO_COMMON_SHARED_LIBRARY "Build Common as shared library" OFF) 
